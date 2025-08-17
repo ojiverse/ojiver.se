b7a707290b26ca023dd4e616e2b5e952c163850f
OldBigBuddha <13755428+OldBigBuddha@users.noreply.github.com>
2025-08-17 19:07:35 +0900

    fix format

--- dns.tf
+++ dns.tf
@@ -17,9 +17,9 @@ resource "cloudflare_record" "cname_api_swarm" {
 }
 
 resource "cloudflare_record" "txt_google_verification" {
-  name = "@"
-  ttl = 3600
-  type = "TXT"
+  name    = "@"
+  ttl     = 3600
+  type    = "TXT"
   content = "google-site-verification=8h_m8FnpdFIiib8-EiTcK8zoRSbuXia0jWcXOXH8PFA"
   zone_id = var.cloudflare_zone_id
 }
